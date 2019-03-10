2c0f679e16f4a3bd1e6c443771a8c7d700be5831
Dawid <dawid.kaleta@interia.pl>
2019-03-10 06:30:27 +0100

    1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Klasy
+{
+    class Program
+    {
+        string napis;
+    public void WczytajNapis()
+    {
+        System.Console.WriteLine("Wprowadź napis.");
+        napis = System.Console.ReadLine();
+    }
+    public void Napisz()
+    {
+        System.Console.Write("Napisałeś -> ");
+        System.Console.WriteLine(napis);
+    }
+}
+class KlasaGlowna
+{
+    static void Main()
+    {
+        Program obiekt = new Program();//new tworzy obiekt na podstawie zdefiniowanej klasy
+        obiekt.WczytajNapis();
+        obiekt.Napisz();
+       // System.Console.ReadLine();
+    }
+    }
+}
